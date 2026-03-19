c65c97bba737386e82bec8641ddae90c031f10e1
Eldiyar <musabaeveldiar@gmail.com>
2026-03-19 18:41:46 +0600

    wip

--- core/src/main/java/kg/musabaev/command/PingCommand.java
+++
@@ -1,7 +0,0 @@
-package kg.musabaev.command;
-
-/**
- * Команда проверки соединения с устройством.
- */
-public non-sealed class PingCommand extends Command {
-}
--- core/src/main/java/kg/musabaev/listener/DoorphoneRingDetectedListener.java
+++
@@ -1,9 +0,0 @@
-package kg.musabaev.listener;
-
-import kg.musabaev.event.DoorphoneRingDetectedEvent;
-
-@FunctionalInterface
-public interface DoorphoneRingDetectedListener {
-
-    void onDetected(DoorphoneRingDetectedEvent event);
-}
