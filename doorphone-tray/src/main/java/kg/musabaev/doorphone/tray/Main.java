d883c5bde3b6ce7f0ed53dde6ad6c686f597e28b
Eldiyar <musabaeveldiar@gmail.com>
2026-03-25 18:19:05 +0600

    feat(tray): add UncaughtExceptionHandler

--- doorphone-tray/src/main/java/kg/musabaev/doorphone/tray/Main.java
+++ doorphone-tray/src/main/java/kg/musabaev/doorphone/tray/Main.java
@@ -1,32 +1,44 @@
 package kg.musabaev.doorphone.tray;
 
 import kg.musabaev.doorphone.core.DeviceServer;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 import java.io.IOException;
 import java.util.concurrent.Executors;
 
+@Slf4j
 public class Main {
 
     private final static int PORT = 9126;
 
     public static void main(String[] args) throws IOException {
-        DeviceServer server = initDeviceServer();
+        // Инициализация DeviceServer
+        var deviceSessionPool = Executors.newSingleThreadExecutor();
+        var commandExecutor = Executors.newVirtualThreadPerTaskExecutor();
+        var server = new DeviceServer(PORT, deviceSessionPool, commandExecutor);
         server.start();
 
+        // Инициализация mDNS
         var jmdns = JmDNS.create();
         var serviceInfo = ServiceInfo.create(
                 "_doorphone._tcp.local.", "doorphone-device-server", PORT, "path=/");
         jmdns.registerService(serviceInfo);
 
+        // Запуск GUI
         var gui = new Gui(server);
         gui.start();
-    }
 
-    private static DeviceServer initDeviceServer() throws IOException {
-        var deviceSessionPool = Executors.newSingleThreadExecutor();
-        var commandExecutor = Executors.newVirtualThreadPerTaskExecutor();
-        return new DeviceServer(PORT, deviceSessionPool, commandExecutor);
+        Thread.currentThread().setUncaughtExceptionHandler((thread, exception) -> {
+            log.error("Uncaught exception", exception);
+            try {
+                server.stop();
+                jmdns.close();
+            } catch (IOException e) {
+                log.error("Error while closing DeviceServer and JmDNS", e);
+            }
+            System.exit(666);
+        });
     }
 }
